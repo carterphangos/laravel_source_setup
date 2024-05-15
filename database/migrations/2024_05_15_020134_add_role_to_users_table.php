31caf9eb5d40b2c11a3f48908a7d258c538b0dab
carterphangos <carter.phan.gos@gmail.com>
2024-05-15 11:33:01 +0700

    modify type role migration

--- database/migrations/2024_05_15_020134_add_role_to_users_table.php
+++ database/migrations/2024_05_15_020134_add_role_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('role')->default(1)->after('password');
+            $table->tinyInteger('role')->default(1)->after('password');
         });
     }
 
