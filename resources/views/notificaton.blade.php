efb32a69673800eb2eeaf25f0c33c0ea23880c9f
carterphangos <carter.phan.gos@gmail.com>
2024-05-15 15:38:48 +0700

    Config Pusher, add event new comment

---
+++ resources/views/notificaton.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<head>
+    <title>Pusher Test</title>
+    <script src="https://js.pusher.com/8.2.0/pusher.min.js"></script>
+    <script>
+        // Enable pusher logging - don't include this in production
+        Pusher.logToConsole = true;
+
+        var pusher = new Pusher('77e33ba790f35b4353d6', {
+            cluster: 'ap1'
+        });
+
+        var channel = pusher.subscribe('comments');
+        channel.bind('new-comment', function(data) {
+            alert(JSON.stringify(data));
+        });
+    </script>
+</head>
+
+<body>
+    <h1>Pusher Test</h1>
+    <p>
+        Try publishing an event to channel <code>my-channel</code>
+        with event name <code>my-event</code>.
+    </p>
+</body>
